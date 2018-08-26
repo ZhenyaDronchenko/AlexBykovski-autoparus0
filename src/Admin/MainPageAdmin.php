5b52177286f9ea6dc4443d3bd6c0336bbf1b8f94
AlexBykovski <bykovski.work@gmail.com>
2018-08-26 20:17:29 +0300

    add homepage

---
+++ src/Admin/MainPageAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Admin;
+
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class MainPageAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('description', CKEditorType::class, ['label' => 'Описание']);
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id', 'text', ['label' => 'ID', 'sortable' => false])
+            ->add('_action', null, [
+                'actions' => [
+                    'edit' => [],
+                ]
+            ]);
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->clearExcept(['list', 'edit']);
+    }
+}
