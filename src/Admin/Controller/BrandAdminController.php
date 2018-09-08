7bb44273aad7d1e68c48685320450737be1de004
AlexBykovski <bykovski.work@gmail.com>
2018-09-08 22:01:38 +0300

    add batch actions

---
+++ src/Admin/Controller/BrandAdminController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Admin\Controller;
+
+use App\Entity\Brand;
+use Doctrine\ORM\EntityManagerInterface;
+use Sonata\AdminBundle\Controller\CRUDController;
+use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class BrandAdminController extends CRUDController
+{
+    public function batchActionAddToIsRelevant()
+    {
+        return true;
+    }
+
+    public function batchActionSetActive(ProxyQueryInterface $selectedModelQuery)
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $selectedBrands = $selectedModelQuery->execute();
+
+        /** @var Brand $brand */
+        foreach ($selectedBrands as $brand){
+            $brand->setActive(true);
+        }
+
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('sonata_flash_success',  sprintf('Выбранные марки теперь активны'));
+
+        return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+
+    public function batchActionSetInActive(ProxyQueryInterface $selectedModelQuery)
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $selectedBrands = $selectedModelQuery->execute();
+
+        /** @var Brand $brand */
+        foreach ($selectedBrands as $brand){
+            $brand->setActive(false);
+        }
+
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('sonata_flash_success',  sprintf('Выбранные марки теперь неактивны'));
+
+        return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+
+    public function batchActionSetPopular(ProxyQueryInterface $selectedModelQuery)
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $selectedBrands = $selectedModelQuery->execute();
+
+        /** @var Brand $brand */
+        foreach ($selectedBrands as $brand){
+            $brand->setPopular(true);
+        }
+
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('sonata_flash_success',  sprintf('Выбранные марки теперь популярны'));
+
+        return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+
+    public function batchActionSetUnPopular(ProxyQueryInterface $selectedModelQuery)
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $selectedBrands = $selectedModelQuery->execute();
+
+        /** @var Brand $brand */
+        foreach ($selectedBrands as $brand){
+            $brand->setPopular(false);
+        }
+
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('sonata_flash_success',  sprintf('Выбранные марки теперь не популярны'));
+
+        return new RedirectResponse($this->admin->generateUrl('list', $this->admin->getFilterParameters()));
+    }
+}
