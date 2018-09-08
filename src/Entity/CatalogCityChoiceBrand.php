e5ba918e21fcae079e99abb541f6ef56a30b45e6
AlexBykovski <bykovski.work@gmail.com>
2018-09-08 23:09:22 +0300

    add city choise city and choice brand into admin

---
+++ src/Entity/CatalogCityChoiceBrand.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="catalog_city_choice_brand")
+ */
+class CatalogCityChoiceBrand
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $description;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $headline1;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="text")
+     */
+    private $text1;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $headline2;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="text")
+     */
+    private $text2;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param null|string $title
+     */
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param null|string $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getHeadline1(): ?string
+    {
+        return $this->headline1;
+    }
+
+    /**
+     * @param null|string $headline1
+     */
+    public function setHeadline1(?string $headline1): void
+    {
+        $this->headline1 = $headline1;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getText1(): ?string
+    {
+        return $this->text1;
+    }
+
+    /**
+     * @param null|string $text1
+     */
+    public function setText1(?string $text1): void
+    {
+        $this->text1 = $text1;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getHeadline2(): ?string
+    {
+        return $this->headline2;
+    }
+
+    /**
+     * @param null|string $headline2
+     */
+    public function setHeadline2(?string $headline2): void
+    {
+        $this->headline2 = $headline2;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getText2(): ?string
+    {
+        return $this->text2;
+    }
+
+    /**
+     * @param null|string $text2
+     */
+    public function setText2(?string $text2): void
+    {
+        $this->text2 = $text2;
+    }
+}
