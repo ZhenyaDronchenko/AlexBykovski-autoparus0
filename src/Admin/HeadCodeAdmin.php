1bfdadcde8aae9e5c295cb1104a99095ad4d5f63
AlexBykovski <bykovski.work@gmail.com>
2018-09-10 22:24:29 +0300

    add editors

---
+++ src/Admin/HeadCodeAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Admin;
+
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class HeadCodeAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('code', CKEditorType::class, ['label' => 'Код']);
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id', 'text', ['label' => 'ID', 'sortable' => false])
+            ->add('_action', null, [
+                'actions' => [
+                    'edit' => [],
+                ]
+            ]);
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->clearExcept(['list', 'edit']);
+    }
+}
