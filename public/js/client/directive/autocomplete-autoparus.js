7b5409c4fc07633b67f19d19bc7415c1e01c7828
AlexBykovski <bykovski.work@gmail.com>
2018-09-25 15:29:15 +0300

    fix autocomplete

--- public/js/client/directive/autocomplete-autoparus.js
+++ public/js/client/directive/autocomplete-autoparus.js
@@ -15,6 +15,12 @@
                     minLength: 1,
                     select: function( event, ui ) {
                         console.log( "Selected: " + ui.item.value);
+                    },
+                    classes: {
+                        "ui-autocomplete": "spare-part-first-autocomplete",
+                    },
+                    open: function(){
+                        $('.ui-autocomplete.spare-part-first-autocomplete').css('width', $("#spare-part-first-autocomplete").width() + 10 + 'px'); // HERE
                     }
                 } );
 
