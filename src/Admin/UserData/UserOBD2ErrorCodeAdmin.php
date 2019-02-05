5378b7a9bb0c623afb5009765e130fa98ee4fbef
AlexBykovski <bykovski.work@gmail.com>
2019-02-05 19:13:31 +0300

    fix user engine and add user errors

---
+++ src/Admin/UserData/UserOBD2ErrorCodeAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Admin\UserData;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class UserOBD2ErrorCodeAdmin extends AbstractAdmin
+{
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('type.designation', TextType::class, ['label' => 'Тип', 'sortable' => false])
+            ->addIdentifier('code', TextType::class, ['label' => 'Код', 'sortable' => false])
+            ->addIdentifier('counter', TextType::class, ['label' => 'Счётчик', 'sortable' => false])
+            ->addIdentifier('approve', null, ['label' => false, 'template' => 'admin/user-data/user-obd2-error-code/agree-button.html.twig', 'sortable' => false])
+            ->addIdentifier('reject', null, ['label' => false, 'template' => 'admin/user-data/user-obd2-error-code/reject-button.html.twig', 'sortable' => false])
+        ;
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->clearExcept(['list']);
+    }
+}
