02ec0d6c18e89cd06204b8b560aadfc899b71027
AlexBykovski <bykovski.work@gmail.com>
2019-05-15 12:07:58 +0300

    remove rollbar including

--- public/js/client/app.js
+++ public/js/client/app.js
@@ -5,21 +5,22 @@
 // });
 
 (function(angular) {
-    window.autoparusApp = angular.module("autoparusApp", ['ngCookies', "ngSanitize", "tandibar/ng-rollbar"]);
+    // window.autoparusApp = angular.module("autoparusApp", ['ngCookies', "ngSanitize", "tandibar/ng-rollbar"]);
+    window.autoparusApp = angular.module("autoparusApp", ['ngCookies', "ngSanitize"]);
 
     autoparusApp.config(function($interpolateProvider){
         $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
     });
 
     //https://github.com/tandibar/ng-rollbar
-    autoparusApp.config(['RollbarProvider', function(RollbarProvider) {
-        RollbarProvider.init({
-            accessToken: "1cb1cea2d9d74134bdbd7be8fde0bcc8",
-            captureUncaught: true,
-            payload: {
-                environment: 'prod'
-            },
-            uncaughtErrorLevel: 'error'
-        });
-    }]);
+    // autoparusApp.config(['RollbarProvider', function(RollbarProvider) {
+    //     RollbarProvider.init({
+    //         accessToken: "1cb1cea2d9d74134bdbd7be8fde0bcc8",
+    //         captureUncaught: true,
+    //         payload: {
+    //             environment: 'prod'
+    //         },
+    //         uncaughtErrorLevel: 'error'
+    //     });
+    // }]);
 })(window.angular);
