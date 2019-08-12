8b1c43df0839437393f94911d02fbd247964ac82
AlexBykovski <bykovski.work@gmail.com>
2019-08-12 18:22:21 +0300

    add product page

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Advert\AutoSparePart\AutoSparePartSpecificAdvert;
+use App\Entity\Article\Article;
+use App\Entity\Brand;
+use App\Entity\City;
+use App\Entity\General\NotFoundPage;
+use App\Entity\Model;
+use App\Entity\SparePart;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+/**
+ * @Route("/product")
+ */
+class ProductController extends Controller
+{
+    /**
+     * @Route("/{urlBrand}/{urlModel}/{urlSP}/{urlCity}", name="product_view")
+     */
+    public function showPostAction(Request $request, $urlBrand, $urlModel, $urlSP, $urlCity)
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+        $sparePart = $em->getRepository(SparePart::class)->findOneBy(["url" => $urlSP]);
+        $brand = $em->getRepository(Brand::class)->findOneBy(["url" => $urlBrand]);
+        $model = $em->getRepository(Model::class)->findOneBy(["url" => $urlModel]);
+        $city = $em->getRepository(City::class)->findOneBy(["url" => $urlCity]);
+        $articles = $em->getRepository(Article::class)->findBy([], ["createdAt" => "DESC"], 2);
+
+        if(!($sparePart instanceof SparePart) || !($brand instanceof Brand) ||
+            !($model instanceof Model) || !($city instanceof City)){
+            throw new NotFoundHttpException(NotFoundPage::DEFAULT_MESSAGE);
+        }
+
+        return $this->render('client/product/product-view.html.twig', [
+            "brand" => $brand,
+            "model" => $model,
+            "sparePart" => $sparePart ?: new SparePart(),
+            "city" => $city,
+            "articles" => $articles,
+        ]);
+    }
+}
