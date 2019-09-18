a841f66af43fb5ad75210bc6b0b09d89abd76ec7
AlexBykovski <bykovski.work@gmail.com>
2019-09-18 22:51:36 +0300

    add comment to publications

--- src/Controller/Catalog/ArticleCatalogController.php
+++ src/Controller/Catalog/ArticleCatalogController.php
@@ -3,14 +3,18 @@
 namespace App\Controller\Catalog;
 
 use App\Entity\Article\Article;
+use App\Entity\Article\ArticleComment;
 use App\Entity\Article\ArticleTheme;
 use App\Entity\Brand;
 use App\Entity\SparePart;
+use App\Helper\ArticleCommentHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class ArticleCatalogController extends Controller
 {
@@ -88,6 +92,62 @@ class ArticleCatalogController extends Controller
             "theme" => $theme ?: new ArticleTheme(),
             "brands" => $parsedBrands,
             "spareParts" => $parsedSpareParts,
+        ]);
+    }
+
+    /**
+     * @Route("/ajax/get-comments/{id}", name="article_catalog_get_comments", options={"expose"=true})
+     *
+     * @ParamConverter("article", class="App\Entity\Article\Article", options={"id" = "id"})
+     */
+    public function getAllCommentsAction(Request $request, Article $article, ArticleCommentHelper $helper)
+    {
+        $comments = $this->getDoctrine()->getRepository(ArticleComment::class)->findBy([
+                "article" => $article,
+                "parent" => null,
+            ]);
+
+        return new JsonResponse($helper->parseComments($comments));
+    }
+
+    /**
+     * @Route("/ajax/add-comments/{id}/", name="article_catalog_send_comment", options={"expose"=true})
+     *
+     * @ParamConverter("article", class="App\Entity\Article\Article", options={"id" = "id"})
+     *
+     * @Security("has_role('ROLE_CLIENT')")
+     */
+    public function sendCommentAction(
+        Request $request,
+        Article $article,
+        ArticleCommentHelper $helper
+    )
+    {
+        /** @var EntityManagerInterface $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $data = json_decode($request->getContent(), true);
+        $text = $data["text"];
+        $parentId = $data["parent"];
+
+        $parent = $parentId ? $em->getRepository(ArticleComment::class)->find($parentId) : null;
+
+        if(!$text || $parent instanceof ArticleComment && $parent->getArticle()->getId() !== $article->getId()){
+            return new JsonResponse([
+                "success" => false,
+                "message" => "Некорретные данные",
+                "text" => $text,
+            ]);
+        }
+
+        $comment = new ArticleComment($text, $article, $this->getUser(), $parent);
+
+        $em->persist($comment);
+        $em->flush();
+
+        return new JsonResponse([
+            "success" => true,
+            "message" => $comment->toArray(),
         ]);
     }
 }
